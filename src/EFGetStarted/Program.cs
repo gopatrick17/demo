a3bf339e2d7255ea25b49fb259ccc096c3add4d8
gopatrick17 <gopatrick17@gmail.com>
2020-07-07 17:18:22 -0500

    add src

---
+++ src/EFGetStarted/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Database.EnsureCreated();
+                // Create
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                db.SaveChanges();
+
+                var test = Console.ReadLine();
+                Console.WriteLine("Enter new ");
+
+                // Read
+                //Console.WriteLine("Querying for a blog");
+                //var blog = db.Blogs
+                //    .OrderBy(b => b.BlogId)
+                //    .First();
+
+                // Update
+                //Console.WriteLine("Updating the blog and adding a post");
+                //blog.Url = "https://devblogs.microsoft.com/dotnet";
+                //blog.Posts.Add(
+                //    new Post
+                //    {
+                //        Title = "Hello World",
+                //        Content = "I wrote an app using EF Core!"
+                //    });
+                db.SaveChanges();
+
+                // Delete
+                //Console.WriteLine("Delete the blog");
+                //db.Remove(blog);
+                //db.SaveChanges();
+            }
+        }
+    }
+}
