21e4a73c3566298aa3dc5a773d2988282ce74a1a
gojanpaolo <go.janpaolo@gmail.com>
2020-07-13 16:51:49 -0500

    Revert "Deleted prev src folder content"
    This reverts commit 7fc6e73cbd6308815ec44846b0bc9b3e982af908.

---
+++ src/EFGetStarted/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Database.EnsureCreated();
+                
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                
+
+                Console.WriteLine("Enter a new blog: ");
+                var test = Console.ReadLine();
+                db.Add(new Blog { Url = $"{test}" });
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
